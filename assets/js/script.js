bd12a346436cbae15fdd5641ce39e8360cc73e45
Omar Ahmad Dahy <82223672+omarahmad101@users.noreply.github.com>
2024-06-17 18:07:38 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const $navbar = document.querySelector("[data-navbar]");
+const $navtoggler = document.querySelector("[data-nav-toggler]");
+
+$navtoggler.addEventListener("click", () => $navbar.classList.toggle('active'))
+
+
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", e => {
+    header.classList[window.scrollY >50 ? "add" : "remove"]("active")
+})
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
